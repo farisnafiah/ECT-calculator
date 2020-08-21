66ffa6d37176a71ae1c29ebe19a5de994cb78c15
Faris Nafiah <nafiahf@lsbu.ac.uk>
2020-08-21 19:03:19 +0100

    Ajax request is still in then mode. Commits to change to success-complete mode

--- index.js
+++ index.js
@@ -7,40 +7,56 @@ const EctSolution = require("./Ect-solution.js");
 const port = 3000;
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
-const mm=1.0e-3, megaSm=1.0e6;
-let ectSoln1 = new EctSolution({r1: 0*mm, r2: 0*mm, z1: 0*mm, z2: 0*mm, wireTurn: 0, cur: 0}, {con1: 0*megaSm, mr1: 0, d: 0*mm}, {con2: 0*megaSm, mr2: 1.0}, [], 100);
+const mm = 1.0e-3,
+  megaSm = 1.0e6;
+let ectSoln1 = new EctSolution({
+  r1: 0 * mm,
+  r2: 0 * mm,
+  z1: 0 * mm,
+  z2: 0 * mm,
+  wireTurn: 0,
+  cur: 0
+}, {
+  con1: 0 * megaSm,
+  mr1: 0,
+  d: 0 * mm
+}, {
+  con2: 0 * megaSm,
+  mr2: 1.0
+}, [], 100);
 
 app.post('/calculation1', (req, res) => {
-  let a = parseInt(req.body.a), b = parseInt(req.body.b), n = parseInt(req.body.n);
-  let r1 = parseInt(req.body.r1), r2 = parseInt(req.body.r2), z1 = parseInt(req.body.z1), z2 = parseInt(req.body.z2);
-  let wireTurn = parseInt(req.body.wireTurn), cur = parseInt(req.body.cur);
-  // let con1 = parseInt(req.body.con1), mr1 = parseInt(req.body.mr1), d = parseInt(req.body.d);
-  let chosenV = req.body.chosenV, mVariable = req.body.mVariable, mat1params = req.body.mat1params;
-  // console.log(req.body.mat1params);
-  // console.log(req.body.mVariable);
-  // console.log(req.body.chosenV);
+  let a = parseInt(req.body.a),
+    b = parseInt(req.body.b),
+    n = parseInt(req.body.n);
+  let r1 = parseInt(req.body.r1),
+    r2 = parseInt(req.body.r2),
+    z1 = parseInt(req.body.z1),
+    z2 = parseInt(req.body.z2);
+  let wireTurn = parseInt(req.body.wireTurn),
+    cur = parseInt(req.body.cur);
+  let chosenV = req.body.chosenV,
+    mVariable = req.body.mVariable,
+    mat1params = req.body.mat1params;
 
   let f = new Array(n).fill(0);
-  // console.log(n);
   f[0] = Math.pow(10, a)
   f.slice(1, f.length).forEach((_f, index) => {
-    return f[index+1] = f[index] * (Math.pow(10, ((b-a)/(n-1)) ))
+    return f[index + 1] = f[index] * (Math.pow(10, ((b - a) / (n - 1))))
   })
-  f[f.length-1] = Math.pow(10, b);
+  f[f.length - 1] = Math.pow(10, b);
   ectSoln1.f = f;
-  ectSoln1.r1 = r1*mm;
-  ectSoln1.r2 = r2*mm;
-  ectSoln1.z1 = z1*mm;
-  ectSoln1.z2 = z2*mm;
+  ectSoln1.r1 = r1 * mm;
+  ectSoln1.r2 = r2 * mm;
+  ectSoln1.z1 = z1 * mm;
+  ectSoln1.z2 = z2 * mm;
   ectSoln1.wireTurn = wireTurn;
   ectSoln1.cur = cur;
-
-  // ectSoln1.con1 = con1*megaSm;
-  // ectSoln1.mr1 = mr1;
-  // ectSoln1.d = d*mm;
 
   let result;
 
@@ -74,27 +90,61 @@ app.post('/calculation1', (req, res) => {
   // }
 
   // if (chosenV === "conductivity") {
-    ectSoln1.con1 = mat1params.con1*megaSm;
-    ectSoln1.mr1 = mat1params.mr1;
+  // ectSoln1.con1 = mat1params.con1 * megaSm;
+  // ectSoln1.mr1 = mat1params.mr1;
 
-    result = mVariable.map( (mV) => {
-      ectSoln1.d = mV*mm;
-      return ectSoln1.getBz();
-    });
+  // result = mVariable.map((mV) => {
+  //   ectSoln1.d = mV * mm;
+  //   return ectSoln1.getBz();
+  // });
   // }
 
+  var do_thing;
+  if (chosenV === "conductivity") {
+    do_thing = () => {
+      ectSoln1.mr1 = mat1params.mr1;
+      ectSoln1.d = mat1params.d *mm;
 
-  // console.log(result);
+      result = mVariable.map((mV) => {
+        ectSoln1.con1 = mV * megaSm;
+        return ectSoln1.getBz();
+      });
+    };
+  } else if (chosenV === "permeability") {
+    do_thing = () => {
+      ectSoln1.con1 = mat1params.con1 * megaSm;
+      ectSoln1.d = mat1params.d *mm;
 
+      result = mVariable.map((mV) => {
+        ectSoln1.mr1 = mV;
+        return ectSoln1.getBz();
+      });
+    };
+  } else {
+    do_thing = () => {
+      ectSoln1.con1 = mat1params.con1 * megaSm;
+      ectSoln1.mr1 = mat1params.mr1;
+
+      result = mVariable.map((mV) => {
+        ectSoln1.d = mV*mm;
+        return ectSoln1.getBz();
+      });
+    };
+  }
+
+  do_thing();
+  
   res.send(JSON.stringify({
     f: f,
     re: result[0].re,
-    im: result[0].im
+    im: result[0].im,
+    result: result
   }));
 
   res.end();
+
 });
 
 app.listen(port, () => {
-    console.log(`in port ${port}`);
+  console.log(`in port ${port}`);
 })
--- public/client1.js
+++ public/client1.js
@@ -1,37 +1,92 @@
+const chartColors = [
+  "#1f77b4",
+  "#ff7f0e",
+  "#2ca02c",
+  "#d62778",
+  "#9467bd",
+  "#8c564b",
+  "#e377c2",
+  "#7f7f7f",
+  "#bcbd22",
+  "#17becf"
+];
+
 var ctx = $('#myChart');
+var ctx2 = $('#myChart2');
 var config = {
   type: 'line',
   data: {
     labels: [],
-    datasets: [{
-      type: 'line',
-      label: "Freq vs real",
-      yAxisID: "y-axis-0",
-      fill: false,
-      borderColor: 'rgba(255, 99, 132, 0.6)',
-      data: [],
-    }, {
-      type: 'line',
-      label: "Freq vs imaginary",
-      yAxisID: "y-axis-1",
-      fill: false,
-      borderColor: 'rgba(0, 0, 255, 0.6)',
-      data: [],
-    }, ]
+    datasets: []
   },
   options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Imaginary Component vs Frequency"
+    },
     scales: {
+      xAxes: [{
+        display: true,
+        ticks: {
+          callback: function(label, index, labels) {
+            return label.toFixed(3)
+          }
+        },
+        scaleLabel: {
+          display: true,
+          labelString: "Frequency, Hz"
+        }
+      }],
       yAxes: [{
-        position: "left",
-        "id": "y-axis-0",
-      }, {
-        position: "right",
-        "id": "y-axis-1",
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: "Imaginary component, V"
+        }
       }]
     }
   }
 };
 var myChart = new Chart(ctx, config);
+var config2 = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: []
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Real Component vs Frequency"
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        ticks: {
+          callback: function(label, index, labels) {
+            return label.toFixed(3)
+          }
+        },
+        scaleLabel: {
+          display: true,
+          labelString: "Frequency, Hz"
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: "Real component, V"
+        }
+      }]
+    }
+  }
+};
+var myChart2 = new Chart(ctx2, config2);
+var myChartConfig = myChart.config;
+var myChartConfig2 = myChart2.config;
 
 function getInputValueAndCalculate() {
 
@@ -39,24 +94,22 @@ function getInputValueAndCalculate() {
   let mVariable;
   let mat1params;
 
-  mVariable = math.range(  parseInt($('#con1-start').val()),parseInt($('#con1-end').val()),parseInt($('#con1-step').val()) )._data;
+  mVariable = math.range(parseInt($('#con1-start').val()), parseInt($('#con1-end').val()), parseInt($('#con1-step').val()))._data;
 
   if (chosenV === "conductivity") {
-    mVariable = math.range(  parseInt($('#con1-start').val()),parseInt($('#con1-end').val()),parseInt($('#con1-step').val()) )._data;
+    mVariable = math.range(parseInt($('#con1-start').val()), parseInt($('#con1-end').val()), parseInt($('#con1-step').val()))._data;
     mat1params = {
       mr1: parseInt($('#mr1').val()),
       d: parseInt($('#d').val())
     };
-  }
-  else if (chosenV === "permeability") {
-    mVariable = math.range(  parseInt($('#mr1-start').val()),parseInt($('#mr1-end').val()),parseInt($('#mr1-step').val()) )._data;
+  } else if (chosenV === "permeability") {
+    mVariable = math.range(parseInt($('#mr1-start').val()), parseInt($('#mr1-end').val()), parseInt($('#mr1-step').val()))._data;
     mat1params = {
       con1: parseInt($('#con1').val()),
       d: parseInt($('#d').val())
     };
-  }
-  else {
-    mVariable = math.range(  parseInt($('#d-start').val()),parseInt($('#d-end').val()),parseInt($('#d-step').val()) )._data;
+  } else {
+    mVariable = math.range(parseInt($('#d-start').val()), parseInt($('#d-end').val()), parseInt($('#d-step').val()))._data;
     mat1params = {
       mr1: parseInt($('#mr1').val()),
       con1: parseInt($('#con1').val())
@@ -67,51 +120,87 @@ function getInputValueAndCalculate() {
     a: parseInt($('#a').val()),
     b: parseInt($('#b').val()),
     n: parseInt($('#n').val()),
-
     r1: parseInt($('#r1').val()),
     r2: parseInt($('#r2').val()),
     z1: parseInt($('#z1').val()),
     z2: parseInt($('#z2').val()),
     wireTurn: parseInt($('#wireTurn').val()),
     cur: parseInt($('#cur').val()),
-
     mVariable: mVariable,
     mat1params: mat1params,
     chosenV: chosenV
-
-    // con1: parseInt($('#con1').val()),
-    // mr1: parseInt($('#mr1').val()),
-    // d: parseInt($('#d').val())
   };
 
   $.ajax({
-    method: 'POST',
-    url: '/calculation1',
-    data: data2Send
-  })
-  .then(
-    response => {
-      let responseParsed = JSON.parse(response);
-      console.log(responseParsed);
-      var myChartConfig = myChart.config;
-      myChartConfig.data.datasets[0].data = responseParsed.re;
-      myChartConfig.data.datasets[1].data = responseParsed.im;
-      myChartConfig.data.labels = responseParsed.f;
-      myChart.update();
-    }
-  )
+      method: 'POST',
+      url: '/calculation1',
+      data: data2Send
+      
+    })
+    .then(
+      response => {
+        let responseParsed = JSON.parse(response);
+        myChartConfig.data.labels = responseParsed.f;
+        myChartConfig2.data.labels = responseParsed.f;
+
+        responseParsed.result.forEach((resul, index) => {
+
+          var newDataset = {
+            label: data2Send.chosenV + " " + data2Send.mVariable[index],
+            backgroundColor: chartColors[myChartConfig.data.datasets.length % chartColors.length],
+            borderColor: chartColors[myChartConfig.data.datasets.length % chartColors.length],
+            data: resul.im,
+            fill: false
+          };
+          updateChart(myChart, myChartConfig, newDataset);
+
+          var newDataset2 = {
+            label: data2Send.chosenV + " " + data2Send.mVariable[index],
+            backgroundColor: chartColors[myChartConfig2.data.datasets.length % chartColors.length],
+            borderColor: chartColors[myChartConfig2.data.datasets.length % chartColors.length],
+            data: resul.re,
+            fill: false
+          };
+          updateChart(myChart2, myChartConfig2, newDataset2);
+        });
+
+      }
+    )
 };
 
-$('#submitButton').on('click', getInputValueAndCalculate);
+function deleteBothChart() {
+  console.log("kjasbv");
+  myChartConfig.data.datasets = [];
+  myChart.update();
+  myChartConfig2.data.datasets = [];
+  myChart2.update();
+}
 
-$("select").change(function(){
-  $(this).find("option:selected").each(function(){
+const updateChart = (whichChart, chartConfig, dataset) => {
+  chartConfig.data.datasets.push(dataset);
+  whichChart.update();
+}
+
+$('#submitButton').on('click', getInputValueAndCalculate);
+$('#deleteButton').on('click', deleteBothChart);
+$("select").change(function() {
+  $(this).find("option:selected").each(function() {
     var optionValue = $(this).attr("value");
     $(".box").not("." + optionValue).hide();
     $("." + optionValue).show();
-
     $(".bbox").not("." + optionValue).show();
-    $(".bbox."+optionValue).hide();
+    $(".bbox." + optionValue).hide();
+
+    if (optionValue === "conductivity") {
+      $("#sampleProp").html(optionValue + " values");
+      $(".sampleProp-unit").html(" MS/m");
+    } else if (optionValue === "thickness") {
+      $("#sampleProp").html(optionValue + " values");
+      $(".sampleProp-unit").html(" mm")
+    } else {
+      $("#sampleProp").html("relative " + optionValue + " values");
+      $(".sampleProp-unit").html("")
+    }
 
   });
 }).change();
